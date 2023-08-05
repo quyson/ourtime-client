0108978f5acbdc50ecb80b3866feb303503b1fa0
unknown <quyson.nguyen.1999@gmail.com>
2023-08-05 15:53:59 -0700

    Create offer method

--- src/VideoRoom.js
+++ src/VideoRoom.js
@@ -4,6 +4,21 @@ import React, {useState, useEffect, useRef} from "react";
 function VideoRoom() {
 
   const [displayVideo, setDisplayVideo] = useState(false);
+  const [localStream, setLocalStream] = useState(null);
+  const [remoteStream, setRemoteStream] = useState(null);
+
+  const createOffer = async () => {
+    let configuration = {'iceServers': [{'urls': 'stun:stun.l.google.com:19302'}]}
+    let peerConnection = new RTCPeerConnection(configuration);
+    let remoteStream = new MediaStream();
+    let remoteVideo = document.querySelector("#remoteVideo");
+    remoteVideo.srcObject = remoteStream;
+
+    let offer = await peerConnection.createOffer();
+    await peerConnection.setLocalDescription(offer);
+
+    document.getElementById("offer").value = JSON.stringify(offer);
+  }
 
   const allowVideo = (e) => {
     setDisplayVideo(true);
@@ -23,6 +38,14 @@ function VideoRoom() {
       <div>
         <button type="button" onClick={allowVideo}>Use Camera</button>
         {displayVideo ? <video playsInline autoPlay muted id="localVideo"></video> : <div>No Video</div>}
+      </div>
+      <div>
+        <video playsInline autoPlay id="remoteVideo"></video>
+      </div>
+      <div>
+        <h1>Offer</h1>
+        <button type="button" onClick={createOffer}>Create an Offer</button>
+        <textarea id="offer"></textarea>
       </div>
     </div>
   );

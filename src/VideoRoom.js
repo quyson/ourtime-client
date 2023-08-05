71cc2921270ed20e5430ef6be536ee9ddec41107
unknown <quyson.nguyen.1999@gmail.com>
2023-08-05 15:25:09 -0700

    Add display video functionality

--- src/VideoRoom.js
+++ src/VideoRoom.js
@@ -3,25 +3,26 @@ import React, {useState, useEffect, useRef} from "react";
 
 function VideoRoom() {
 
-  
+  const [displayVideo, setDisplayVideo] = useState(false);
+
+  const allowVideo = (e) => {
+    setDisplayVideo(true);
+  }
+
   useEffect(() => {
-    navigator.mediaDevices.getUserMedia({video: true, audio: true}).then((stream) => {
-      const local = document.querySelector("#localVideo");
-      local.srcObject = stream;
-    })
-  }, [])
-
-  /*navigator.mediaDevices.getUserMedia({video: true, audio: true})
-    .then((stream) => {
-      const myVideo = document.querySelector("#local");
-      myVideo.srcObject = stream;
-    });*/
+    if(displayVideo){
+      navigator.mediaDevices.getUserMedia({video: true, audio: true}).then((stream) => {
+        const myLocalVideo = document.querySelector("#localVideo");
+        myLocalVideo.srcObject = stream;
+      })
+    }
+  }, [displayVideo])
  
-
   return (
     <div>
       <div>
-        <video playsInline autoPlay muted id="localVideo"></video>
+        <button type="button" onClick={allowVideo}>Use Camera</button>
+        {displayVideo ? <video playsInline autoPlay muted id="localVideo"></video> : <div>No Video</div>}
       </div>
     </div>
   );
